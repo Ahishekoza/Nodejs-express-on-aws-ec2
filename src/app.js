a85742a410e42687c45197da4bb5a1937ee7abb1
Abhishek Oza <101032683+Ahishekoza@users.noreply.github.com>
2023-03-30 12:44:52 +0530

    Sample Code to Test CI/CD

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:'You have successfully created a new project'
+    })
+})
+
+app.get('/products',(req,res)=>{
+    res.status(200).json({
+        Products:[
+            {
+                productId:1,
+                productName:'Oven',
+                productPrice:20000
+            },
+            {
+                productId:1,
+                productName:'Refrigerator',
+                productPrice:250000
+            }
+        ]
+    })
+})
+
+app.listen(8080,()=>{
+    console.log('listening on port 8080')
+})
